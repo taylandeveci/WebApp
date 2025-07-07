7ae56e6dd5cdbc1fc9e1c5968fbcb7de3de27fee
Taylan Deveci <87483564+taylandeveci@users.noreply.github.com>
2025-07-07 15:34:25 +0300

    Add files via upload

---
+++ Interfaces/IOwnerRepository.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Interfaces
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(int ownerId);
+        ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExists(int ownerId);
+        bool CreateOwner(Owner owner);
+        bool UpdateOwner(Owner owner);
+        bool DeleteOwner(Owner owner);
+        bool Save();
+    }
+}
